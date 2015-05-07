68971879342e7b4431d748a9ff6a5f4d47cc602a
Adrian Hunter <adrian.hunter@intel.com>
2015-05-07 13:10:12 +0300

    mmc: host: Add facility to support re-tuning
    Currently, there is core support for tuning during
    initialization. There can also be a need to re-tune
    periodically (e.g. sdhci) or to re-tune after the
    host controller is powered off (e.g. after PM
    runtime suspend / resume) or to re-tune in response
    to CRC errors.
    
    The main requirements for re-tuning are:
      - ability to enable / disable re-tuning
      - ability to flag that re-tuning is needed
      - ability to re-tune before any request
      - ability to hold off re-tuning if the card is busy
      - ability to hold off re-tuning if re-tuning is in
      progress
      - ability to run a re-tuning timer
    
    To support those requirements 7 members are added to struct
    mmc_host:
    
      unsigned int		can_retune:1;	/* re-tuning can be used */
      unsigned int		doing_retune:1;	/* re-tuning in progress */
      unsigned int		retune_now:1;   /* do re-tuning at next req */
      int			need_retune;	/* re-tuning is needed */
      int			hold_retune;	/* hold off re-tuning */
      unsigned int		retune_period;  /* re-tuning period in secs */
      struct timer_list	retune_timer;	/* for periodic re-tuning */
    
    need_retune is an integer so it can be set without needing
    synchronization. hold_retune is a integer to allow nesting.
    
    Various simple functions are provided to set / clear those
    variables.
    
    Subsequent patches take those functions into use.
    
    Signed-off-by: Adrian Hunter <adrian.hunter@intel.com>
    Signed-off-by: Ulf Hansson <ulf.hansson@linaro.org>
    Git-commit: dfa13ebbe3340e538b988f5608efd9ff2ca7fc35
    Git-repo: git://git.linaro.org/people/ulf.hansson/mmc.git
    Change-Id: I82fa292b96ee57f18dd089287940aaca5286a304
    Signed-off-by: Veerabhadrarao Badiganti <vbadigan@codeaurora.org>

--- drivers/mmc/core/host.h
+++ drivers/mmc/core/host.h
@@ -16,9 +16,13 @@
 
 int mmc_register_host_class(void);
 void mmc_unregister_host_class(void);
-
 void mmc_latency_hist_sysfs_init(struct mmc_host *host);
 void mmc_latency_hist_sysfs_exit(struct mmc_host *host);
+void mmc_retune_enable(struct mmc_host *host);
+void mmc_retune_disable(struct mmc_host *host);
+void mmc_retune_hold(struct mmc_host *host);
+void mmc_retune_release(struct mmc_host *host);
+int mmc_retune(struct mmc_host *host);
 
 #endif
 
