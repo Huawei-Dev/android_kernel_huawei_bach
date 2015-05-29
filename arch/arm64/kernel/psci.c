37c343680d4c4f51d9cd92ae4649fe3c464069a5
Will Deacon <will.deacon@arm.com>
2015-05-29 18:28:44 +0100

    BACKPORT: arm64: force CONFIG_SMP=y and remove redundant #ifdefs
    Nobody seems to be producing !SMP systems anymore, so this is just
    becoming a source of kernel bugs, particularly if people want to use
    coherent DMA with non-shared pages.
    
    This patch forces CONFIG_SMP=y for arm64, removing a modest amount of
    code in the process.
    
    Signed-off-by: Will Deacon <will.deacon@arm.com>
    
    Bug: 31432001
    Change-Id: Ib3e05c90d2f7699d537bc09c2d7991c7241b7dae
    (cherry picked from commit 4b3dc9679cf779339d9049800803dfc3c83433d1)
    Signed-off-by: Sami Tolvanen <samitolvanen@google.com>

--- arch/arm64/kernel/psci.c
+++ arch/arm64/kernel/psci.c
@@ -436,8 +436,6 @@ int __init psci_init(void)
 	return init_fn(np);
 }
 
-#ifdef CONFIG_SMP
-
 static int __init cpu_psci_cpu_init(struct device_node *dn, unsigned int cpu)
 {
 	pr_info("Initializing psci_cpu_init\n");
@@ -517,7 +515,6 @@ static int cpu_psci_cpu_kill(unsigned int cpu)
 	return 0;
 }
 #endif
-#endif
 
 static int psci_suspend_finisher(unsigned long state_id)
 {
@@ -541,7 +538,6 @@ static struct cpu_operations cpu_psci_ops = {
 	.cpu_init_idle	= cpu_psci_cpu_init_idle,
 	.cpu_suspend	= cpu_psci_cpu_suspend,
 #endif
-#ifdef CONFIG_SMP
 	.cpu_init	= cpu_psci_cpu_init,
 #ifdef CONFIG_ARM64_CPU_SUSPEND
 	.cpu_suspend	= cpu_psci_cpu_suspend,
@@ -552,7 +548,6 @@ static struct cpu_operations cpu_psci_ops = {
 	.cpu_disable	= cpu_psci_cpu_disable,
 	.cpu_die	= cpu_psci_cpu_die,
 	.cpu_kill	= cpu_psci_cpu_kill,
-#endif
 #endif
 };
 CPU_METHOD_OF_DECLARE(psci, "psci", &cpu_psci_ops);
