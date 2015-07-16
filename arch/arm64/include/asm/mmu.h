a55c9676f30ae2901cf68e0e75d633f363987333
Mark Salter <msalter@redhat.com>
2015-07-16 18:58:53 +0100

    UPSTREAM: arm64: remove dead code
    Commit 68234df4ea79 ("arm64: kill flush_cache_all()") removed
    soft_reset() from the kernel. This was the only caller of
    setup_mm_for_reboot(), so remove that also.
    
    Signed-off-by: Mark Salter <msalter@redhat.com>
    Signed-off-by: Will Deacon <will.deacon@arm.com>
    
    Bug: 31432001
    Change-Id: Ic138d2e53449c73bf4d289f5e33bb04588e40baf
    (cherry picked from commit b08d4640a3dca68670fc5af2fe9205b395a02388)
    Signed-off-by: Sami Tolvanen <samitolvanen@google.com>

--- arch/arm64/include/asm/mmu.h
+++ arch/arm64/include/asm/mmu.h
@@ -28,7 +28,6 @@ typedef struct {
 #define ASID(mm)	((mm)->context.id & 0xffff)
 
 extern void paging_init(void);
-extern void setup_mm_for_reboot(void);
 extern void __iomem *early_io_map(phys_addr_t phys, unsigned long virt);
 extern void init_mem_pgprot(void);
 extern void mem_text_write_kernel_word(u32 *addr, u32 word);
