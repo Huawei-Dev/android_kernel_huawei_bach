6206ea9155de719012d7ac06f768720062deff56
Ghanim Fodi <gfodi@codeaurora.org>
2016-10-05 11:59:18 +0300

    msm: ipa3: Support IPA-USB suspend sequence without remote wake-up
    Today IPA-USB uses disconnect sequence when USB initiates
    suspend without remote wake-up, and uses suspend sequence
    when USB initiates suspend with remote wake-up.
    In accordance with USB-GSI driver, changing this behavior
    to use suspend sequence in both of the cases. This is to
    overcome some USB H/W malfunction when using disconnect
    sequence in case of suspend scenario.
    
    Change-Id: Iba43e192905ab37d196aea4b5d00861b9cb7c347
    CRs-Fixed: 1068949
    Signed-off-by: Ghanim Fodi <gfodi@codeaurora.org>

--- drivers/usb/gadget/function/f_gsi.c
+++ drivers/usb/gadget/function/f_gsi.c
@@ -971,7 +971,7 @@ static int ipa_suspend_work_handler(struct gsi_data_port *d_port)
 	log_event_dbg("%s: Calling xdci_suspend", __func__);
 	ret = ipa_usb_xdci_suspend(gsi->d_port.out_channel_handle,
 				gsi->d_port.in_channel_handle, gsi->prot_id,
-				usb_gsi_remote_wakeup_allowed(f));
+				true);
 
 	if (!ret) {
 		d_port->sm_state = STATE_SUSPENDED;
