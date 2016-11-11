e481ee57788e0c14417d0e36d6dac3cff1ed5bce
Honghao Liu <honghaol@codeaurora.org>
2016-11-11 16:02:27 -0500

    ASoC: msm: qdsp6v2: add route for secondary MI2S TX hostless capture
    Add the route from secondary MI2S TX BE to secondary MI2S TX hostless
    capture FE.
    
    CRs-fixed: 1087945
    
    Change-Id: Id8245c4caa831cf6f01b0e2102c8883fe0fcee92
    Signed-off-by: Honghao Liu <honghaol@codeaurora.org>

--- sound/soc/msm/qdsp6v2/msm-pcm-routing-v2.c
+++ sound/soc/msm/qdsp6v2/msm-pcm-routing-v2.c
@@ -9644,6 +9644,7 @@ static const struct snd_soc_dapm_route intercon[] = {
 	{"TERT_MI2S_UL_HL", NULL, "TERT_MI2S_TX"},
 	{"SEC_I2S_RX", NULL, "SEC_I2S_DL_HL"},
 	{"PRI_MI2S_UL_HL", NULL, "PRI_MI2S_TX"},
+	{"SEC_MI2S_UL_HL", NULL, "SEC_MI2S_TX"},
 	{"SEC_MI2S_RX", NULL, "SEC_MI2S_DL_HL"},
 	{"PRI_MI2S_RX", NULL, "PRI_MI2S_DL_HL"},
 	{"QUIN_MI2S_RX_DL_HL", "Switch", "PRI_MI2S_DL_HL"},
