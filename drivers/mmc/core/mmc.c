7898a4978f8c4a1437699e6ed86fcde8ad9ea05a
Sayali Lokhande <sayalil@codeaurora.org>
2016-12-12 08:48:37 +0530

    mmc: core: Add mmc tracing events in suspend and resume
    Add mmc tracing events in suspend and resume.
    
    Change-Id: I5ca1c3f667003c4b4224b65d2a5db50766b44d75
    Signed-off-by: Sayali Lokhande <sayalil@codeaurora.org>

--- drivers/mmc/core/mmc.c
+++ drivers/mmc/core/mmc.c
@@ -2565,6 +2565,7 @@ int mmc_suspend(struct mmc_host *host)
 	int err;
 	ktime_t start = ktime_get();
 
+	MMC_TRACE(host, "%s: Enter\n", __func__);
 	err = _mmc_suspend(host, true);
 	if (!err) {
 		pm_runtime_disable(&host->card->dev);
@@ -2573,6 +2574,7 @@ int mmc_suspend(struct mmc_host *host)
 
 	trace_mmc_suspend(mmc_hostname(host), err,
 			ktime_to_us(ktime_sub(ktime_get(), start)));
+	MMC_TRACE(host, "%s: Exit err: %d\n", __func__, err);
 	return err;
 }
 
@@ -2648,6 +2650,7 @@ static int mmc_resume(struct mmc_host *host)
 	int err = 0;
 	ktime_t start = ktime_get();
 
+	MMC_TRACE(host, "%s: Enter\n", __func__);
 	if (!(host->caps & MMC_CAP_RUNTIME_RESUME)) {
 		err = _mmc_resume(host);
 		pm_runtime_set_active(&host->card->dev);
@@ -2657,7 +2660,7 @@ static int mmc_resume(struct mmc_host *host)
 
 	trace_mmc_resume(mmc_hostname(host), err,
 			ktime_to_us(ktime_sub(ktime_get(), start)));
-
+	MMC_TRACE(host, "%s: Exit err: %d\n", __func__, err);
 	return err;
 }
 
