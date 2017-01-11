e1cf9141f44ff939d4c3299e6c9b3765f4ce8714
Ashish Garg <ashigarg@codeaurora.org>
2017-01-11 18:42:25 +0530

    msm: mdss: add hdmi support on apq8017 smart audio platform
    Add hpd, interface mux gpios, adv interface node to support
    hdmi on apq8017 smart audio platform.
    
    Change-Id: Ia2661f6dba355128bf398ef05d55bdea71681121
    Signed-off-by: Ashish Garg <ashigarg@codeaurora.org>

--- drivers/video/msm/mdss/mdss_dsi.h
+++ drivers/video/msm/mdss/mdss_dsi.h
@@ -1,4 +1,4 @@
-/* Copyright (c) 2012-2016, The Linux Foundation. All rights reserved.
+/* Copyright (c) 2012-2017, The Linux Foundation. All rights reserved.
  *
  * This program is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License version 2 and
@@ -444,6 +444,7 @@ struct mdss_dsi_ctrl_pdata {
 
 	int bklt_en_gpio;
 	int mode_gpio;
+	int intf_mux_gpio;
 	int bklt_ctrl;	/* backlight ctrl */
 	bool pwm_pmi;
 	int pwm_period;
