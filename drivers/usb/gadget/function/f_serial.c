a57cbc87b85c9e54672c0e3508eae10e7ac15b35
Manu Gautam <mgautam@codeaurora.org>
2017-01-20 11:40:45 +0530

    usb: gadget: replace %p with %pK
    Format specifier %p can leak kernel addresses while not valuing the
    kptr_restrict system settings. When kptr_restrict is set to (1), kernel
    pointers printed using the %pK format specifier will be replaced with 0's.
    Debugging Note : &pK prints only Zeros as address. If you need actual
    address information, write 0 to kptr_restrict.
    
    echo 0 > /proc/sys/kernel/kptr_restrict
    
    CRs-fixed: 1052849
    Change-Id: Ice16947924f56b8f4244ae03b9ab4f2e36b7d56b
    Signed-off-by: Manu Gautam <mgautam@codeaurora.org>

--- drivers/usb/gadget/function/f_serial.c
+++ drivers/usb/gadget/function/f_serial.c
@@ -4,7 +4,7 @@
  * Copyright (C) 2003 Al Borchers (alborchers@steinerpoint.com)
  * Copyright (C) 2008 by David Brownell
  * Copyright (C) 2008 by Nokia Corporation
- * Copyright (c) 2014-2016, The Linux Foundation. All rights reserved.
+ * Copyright (c) 2014-2017, The Linux Foundation. All rights reserved.
  *
  * This software is distributed under the terms of the GNU General
  * Public License ("GPL") as published by the Free Software Foundation,
@@ -411,7 +411,7 @@ static int gport_connect(struct f_gser *gser)
 	unsigned	port_num;
 	int		ret;
 
-	pr_debug("%s: transport: %s f_gser: %p gserial: %p port_num: %d\n",
+	pr_debug("%s: transport: %s f_gser: %pK gserial: %pK port_num: %d\n",
 			__func__, xport_to_str(gser->transport),
 			gser, &gser->port, gser->port_num);
 
@@ -457,7 +457,7 @@ static int gport_disconnect(struct f_gser *gser)
 
 	port_num = gserial_ports[gser->port_num].client_port_num;
 
-	pr_debug("%s: transport: %s f_gser: %p gserial: %p port_num: %d\n",
+	pr_debug("%s: transport: %s f_gser: %pK gserial: %pK port_num: %d\n",
 			__func__, xport_to_str(gser->transport),
 			gser, &gser->port, gser->port_num);
 
@@ -658,7 +658,7 @@ static void gser_suspend(struct usb_function *f)
 
 	port_num = gserial_ports[gser->port_num].client_port_num;
 
-	pr_debug("%s: transport: %s f_gser: %p gserial: %p port_num: %d\n",
+	pr_debug("%s: transport: %s f_gser: %pK gserial: %pK port_num: %d\n",
 			__func__, xport_to_str(gser->transport),
 			gser, &gser->port, gser->port_num);
 
@@ -680,7 +680,7 @@ static void gser_resume(struct usb_function *f)
 
 	port_num = gserial_ports[gser->port_num].client_port_num;
 
-	pr_debug("%s: transport: %s f_gser: %p gserial: %p port_num: %d\n",
+	pr_debug("%s: transport: %s f_gser: %pK gserial: %pK port_num: %d\n",
 			__func__, xport_to_str(gser->transport),
 			gser, &gser->port, gser->port_num);
 	/*
