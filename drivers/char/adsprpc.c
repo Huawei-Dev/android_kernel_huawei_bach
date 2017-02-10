0fd5f154f99d75dae384e1a1b4134f108b22f299
Sathish Ambley <sathishambley@codeaurora.org>
2017-02-10 10:25:36 -0800

    msm: ADSPRPC: Fix leak that happens while the list gets empty
    When the list that holds the open file handles gets empty, the
    node does not get freed causing a leak.
    
    Change-Id: I611387abd27033d4ded63b3bf271162c5d95653d
    Acked-by: Chenna Kesava Raju <chennak@qti.qualcomm.com>
    Signed-off-by: Sathish Ambley <sathishambley@codeaurora.org>

--- drivers/char/adsprpc.c
+++ drivers/char/adsprpc.c
@@ -2023,8 +2023,10 @@ static void file_free_work_handler(struct work_struct *w)
 			break;
 		}
 		mutex_unlock(&me->flfree_mutex);
-		if (freefl)
+		if (freefl) {
 			fastrpc_file_free(freefl->fl);
+			kfree(freefl);
+		}
 		mutex_lock(&me->flfree_mutex);
 
 		if (hlist_empty(&me->fls)) {
@@ -2034,7 +2036,6 @@ static void file_free_work_handler(struct work_struct *w)
 			break;
 		}
 		mutex_unlock(&me->flfree_mutex);
-		kfree(freefl);
 	}
 	return;
 }
