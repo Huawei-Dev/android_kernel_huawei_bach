7a64bc734a998b660a5ca51564292a4214dff6b5
Arnd Bergmann <arnd@arndb.de>
2017-05-09 21:46:59 +0200

    Revert "cpu: add generic support for CPU feature based module autoloading"
    This reverts commit 9025d688a3bcd3c21d4b5dca9a8cd4b8b924f06b.
    
    A patch was accidentally applied into android-3.18 even though it was
    already merged in 3.15 earlier, and partly reverted later by commit
    2b9c1f03278a ("x86: align x86 arch with generic CPU modalias handling")
    
    Applying it again resulted in an inconsistent state of the Kconfig
    variables that led to the feature being disabled on all architectures,
    as shown by this warning:
    
    drivers/base/cpu.c:317:12: warning: 'cpu_uevent' defined but not used [-Wunused-function]
    
    This reverts the incorrectly applied patch.
    
    Fixes: 9025d688a3bc ("cpu: add generic support for CPU feature based module autoloading")
    Cc: Ard Biesheuvel <ard.biesheuvel@linaro.org>
    Signed-off-by: Arnd Bergmann <arnd@arndb.de>
    Signed-off-by: Greg Kroah-Hartman <gregkh@google.com>

--- drivers/base/cpu.c
+++ drivers/base/cpu.c
@@ -559,7 +559,6 @@ static void cpu_device_release(struct device *dev)
 	 */
 }
 
-#ifdef CONFIG_HAVE_CPU_AUTOPROBE
 #ifdef CONFIG_GENERIC_CPU_AUTOPROBE
 static ssize_t print_cpu_modalias(struct device *dev,
 				  struct device_attribute *attr,
@@ -582,11 +581,8 @@ static ssize_t print_cpu_modalias(struct device *dev,
 	buf[n++] = '\n';
 	return n;
 }
-#else
-#define print_cpu_modalias	arch_print_cpu_modalias
-#endif
 
-#ifdef CONFIG_HAVE_CPU_AUTOPROBE
+#ifdef CONFIG_GENERIC_CPU_AUTOPROBE
 static int cpu_uevent(struct device *dev, struct kobj_uevent_env *env)
 {
 	char *buf = kzalloc(PAGE_SIZE, GFP_KERNEL);
@@ -597,7 +593,7 @@ static int cpu_uevent(struct device *dev, struct kobj_uevent_env *env)
 	}
 	return 0;
 }
-#endif /*CONFIG_HAVE_CPU_AUTOPROBE*/
+#endif /*CONFIG_GENERIC_CPU_AUTOPROBE*/
 #endif
 
 /*
@@ -620,7 +616,7 @@ int register_cpu(struct cpu *cpu, int num)
 	cpu->dev.offline_disabled = !cpu->hotpluggable;
 	cpu->dev.offline = !cpu_online(num);
 	cpu->dev.of_node = of_get_cpu_node(num, NULL);
-#ifdef CONFIG_HAVE_CPU_AUTOPROBE
+#ifdef CONFIG_GENERIC_CPU_AUTOPROBE
 	cpu->dev.bus->uevent = cpu_uevent;
 #endif
 	cpu->dev.groups = common_cpu_attr_groups;
@@ -644,7 +640,7 @@ struct device *get_cpu_device(unsigned cpu)
 }
 EXPORT_SYMBOL_GPL(get_cpu_device);
 
-#ifdef CONFIG_HAVE_CPU_AUTOPROBE
+#ifdef CONFIG_GENERIC_CPU_AUTOPROBE
 static DEVICE_ATTR(modalias, 0444, print_cpu_modalias, NULL);
 #endif
 
@@ -658,7 +654,7 @@ static struct attribute *cpu_root_attrs[] = {
 	&cpu_attrs[2].attr.attr,
 	&dev_attr_kernel_max.attr,
 	&dev_attr_offline.attr,
-#ifdef CONFIG_HAVE_CPU_AUTOPROBE
+#ifdef CONFIG_GENERIC_CPU_AUTOPROBE
 	&dev_attr_modalias.attr,
 #endif
 	NULL
