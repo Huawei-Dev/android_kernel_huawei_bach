ebc036d5267488980ec5169170e6f8ffc3b057e6
Sai Krishna Juturi <jsaikrishna@codeaurora.org>
2017-06-01 10:00:48 +0530

    usb: dwc3: Add check for SDP charger type
    During boot up when device is connected to CDP,
    driver is checking for floating charger, which is not required
    as PMIC has notified appropriate charger type already.
    So, check for floating charger only in case of SDP.
    
    Change-Id: Ia0b37742f86e2374eaeb1b06870f1c7a9ac01cb1
    CRs-fixed: 2053529
    Signed-off-by: Sai Krishna Juturi <jsaikrishna@codeaurora.org>

--- drivers/usb/dwc3/dwc3-msm.c
+++ drivers/usb/dwc3/dwc3-msm.c
@@ -3987,7 +3987,8 @@ static void dwc3_msm_otg_sm_work(struct work_struct *w)
 				}
 				dwc3_msm_gadget_vbus_draw(mdwc, USB_SDP_CURRENT);
 				/* check dp/dm for SDP & runtime_put if !SDP */
-				if (mdwc->detect_dpdm_floating) {
+				if (mdwc->detect_dpdm_floating &&
+					mdwc->chg_type == DWC3_SDP_CHARGER) {
 					dwc3_check_float_lines(mdwc);
 					if (mdwc->chg_type != DWC3_SDP_CHARGER)
 						break;
