92d2ba884d486527f480dc784618ba9c3d966fad
Surendar karka <sukark@codeaurora.org>
2017-08-01 12:04:35 +0530

    ASoC: msm: add afe_loopback_tx BE dai
    Add afe_loopback_tx dai driver and routing
    driver changes for afe_loopback_tx back
    end dai.
    
    CRs-Fixed: 2080421
    Change-Id: Ia57f335bde5f8a47de0030fbf1cdb784c8821e17
    Signed-off-by: Surendar karka <sukark@codeaurora.org>

--- sound/soc/msm/qdsp6v2/q6afe.c
+++ sound/soc/msm/qdsp6v2/q6afe.c
@@ -667,6 +667,7 @@ int afe_get_port_type(u16 port_id)
 	case AFE_PORT_ID_QUATERNARY_TDM_TX_5:
 	case AFE_PORT_ID_QUATERNARY_TDM_TX_6:
 	case AFE_PORT_ID_QUATERNARY_TDM_TX_7:
+	case AFE_LOOPBACK_TX:
 		ret = MSM_AFE_PORT_TYPE_TX;
 		break;
 
@@ -3150,6 +3151,8 @@ int afe_get_port_index(u16 port_id)
 		return IDX_AFE_PORT_ID_QUATERNARY_TDM_RX_7;
 	case AFE_PORT_ID_QUATERNARY_TDM_TX_7:
 		return IDX_AFE_PORT_ID_QUATERNARY_TDM_TX_7;
+	case AFE_LOOPBACK_TX:
+		return IDX_AFE_LOOPBACK_TX;
 	default:
 		pr_err("%s: port 0x%x\n", __func__, port_id);
 		return -EINVAL;
