b12891c1cb0a21d617002950807013c40fcfcf88
Ajay Agarwal <ajaya@codeaurora.org>
2017-08-09 12:48:51 +0530

    gpio-usbdetect: Add debounce in VBUS detection handling
    Currently the GPIO usbdetect driver calls set_property
    as soon as it receives VBUS detection interrupt. If the
    time between VBUS off and on is very less, this can cause
    set_property and otg_sm_work to race, thereby causing
    syncronisation issue and enumeration failure.
    
    To fix this behavior, add some debounce time for VBUS
    'on' case in the IRQ handler and move the charger
    notification to bottom half.
    
    Change-Id: I59da3b3e4bd639d2a79934d9cf93e4da29b2f46d
    Signed-off-by: Ajay Agarwal <ajaya@codeaurora.org>

--- drivers/platform/msm/gpio-usbdetect.c
+++ drivers/platform/msm/gpio-usbdetect.c
@@ -21,28 +21,66 @@
 #include <linux/power_supply.h>
 #include <linux/regulator/consumer.h>
 
+#define VBUS_ON_DEBOUNCE_MS		400
+#define WAKEUP_SRC_TIMEOUT_MS		1000
+
 struct gpio_usbdetect {
 	struct platform_device	*pdev;
 	struct regulator	*vin;
 	struct power_supply	*usb_psy;
 	int			vbus_det_irq;
+	struct delayed_work     chg_work;
+	int                     vbus;
+	struct regulator	*vdd33;
+	struct regulator	*vdd12;
+	int			gpio_usbdetect;
+	bool			notify_host_mode;
+	bool			disable_device_mode;
 };
 
 static irqreturn_t gpio_usbdetect_vbus_irq(int irq, void *data)
 {
 	struct gpio_usbdetect *usb = data;
-	int vbus;
 
-	vbus = !!irq_read_line(irq);
-	if (vbus)
-		power_supply_set_supply_type(usb->usb_psy,
-				POWER_SUPPLY_TYPE_USB);
+	if (gpio_is_valid(usb->gpio_usbdetect))
+		usb->vbus = gpio_get_value(usb->gpio_usbdetect);
 	else
+		usb->vbus = !!irq_read_line(irq);
+
+	pm_wakeup_event(&usb->pdev->dev, WAKEUP_SRC_TIMEOUT_MS);
+	if (!usb->vbus)
+		schedule_delayed_work(&usb->chg_work, 0);
+	else
+		schedule_delayed_work(&usb->chg_work,
+					msecs_to_jiffies(VBUS_ON_DEBOUNCE_MS));
+
+	return IRQ_HANDLED;
+}
+
+static void gpio_usbdetect_chg_work(struct work_struct *w)
+{
+	struct gpio_usbdetect *usb = container_of(w, struct gpio_usbdetect,
+							chg_work.work);
+
+	if (usb->vbus) {
+		if (usb->notify_host_mode)
+			power_supply_set_usb_otg(usb->usb_psy, 0);
+
+		if (!usb->disable_device_mode) {
+			power_supply_set_supply_type(usb->usb_psy,
+						POWER_SUPPLY_TYPE_USB);
+			power_supply_set_present(usb->usb_psy, usb->vbus);
+		}
+	} else {
+		/* notify gpio_state = LOW as disconnect */
 		power_supply_set_supply_type(usb->usb_psy,
 				POWER_SUPPLY_TYPE_UNKNOWN);
+		power_supply_set_present(usb->usb_psy, usb->vbus);
 
-	power_supply_set_present(usb->usb_psy, vbus);
-	return IRQ_HANDLED;
+		/* Cheeck if low gpio_state be treated as HOST mode */
+		if (usb->notify_host_mode)
+			power_supply_set_usb_otg(usb->usb_psy, 1);
+	}
 }
 
 static int gpio_usbdetect_probe(struct platform_device *pdev)
@@ -65,23 +103,14 @@ static int gpio_usbdetect_probe(struct platform_device *pdev)
 	usb->pdev = pdev;
 	usb->usb_psy = usb_psy;
 
-	if (of_get_property(pdev->dev.of_node, "vin-supply", NULL)) {
-		usb->vin = devm_regulator_get(&pdev->dev, "vin");
-		if (IS_ERR(usb->vin)) {
-			dev_err(&pdev->dev, "Failed to get VIN regulator: %ld\n",
-				PTR_ERR(usb->vin));
-			return PTR_ERR(usb->vin);
-		}
-	}
-
-	if (usb->vin) {
-		rc = regulator_enable(usb->vin);
-		if (rc) {
-			dev_err(&pdev->dev, "Failed to enable VIN regulator: %d\n",
-				rc);
-			return rc;
-		}
-	}
+	INIT_DELAYED_WORK(&usb->chg_work, gpio_usbdetect_chg_work);
+	usb->notify_host_mode = of_property_read_bool(pdev->dev.of_node,
+					"qcom,notify-host-mode");
+	usb->disable_device_mode = of_property_read_bool(pdev->dev.of_node,
+					"qcom,disable-device-mode");
+	rc = gpio_enable_ldos(usb, 1);
+	if (rc)
+		return rc;
 
 	usb->vbus_det_irq = platform_get_irq_byname(pdev, "vbus_det_irq");
 	if (usb->vbus_det_irq < 0) {
@@ -102,6 +131,8 @@ static int gpio_usbdetect_probe(struct platform_device *pdev)
 		return rc;
 	}
 
+	device_init_wakeup(&pdev->dev, 1);
+
 	enable_irq_wake(usb->vbus_det_irq);
 	dev_set_drvdata(&pdev->dev, usb);
 
@@ -117,10 +148,11 @@ static int gpio_usbdetect_remove(struct platform_device *pdev)
 {
 	struct gpio_usbdetect *usb = dev_get_drvdata(&pdev->dev);
 
+	device_wakeup_disable(&usb->pdev->dev);
 	disable_irq_wake(usb->vbus_det_irq);
 	disable_irq(usb->vbus_det_irq);
-	if (usb->vin)
-		regulator_disable(usb->vin);
+	gpio_enable_ldos(usb, 0);
+	cancel_delayed_work_sync(&usb->chg_work);
 
 	return 0;
 }
