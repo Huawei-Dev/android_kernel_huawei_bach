25358571613f402c57a261e2a25c987aee9c69fc
Lingutla Chandrasekhar <clingutla@codeaurora.org>
2017-09-12 22:24:55 +0530

    arm64: Add support to print hardware id info
    Initial support to print hardware id is added with
    commit 'eab25833f0d4 ("ARM/ARM64: Introduce
    arch_read_hardware_id")', with recent down port
    changes, this print hardware info is missed to propagate.
    Adding it back with defining common variables in header file.
    
    Change-Id: I4da63bd2346c0d0296087941967ae1a98eb6d86f
    Signed-off-by: Abhimanyu Kapur <abhimany@codeaurora.org>
    Signed-off-by: Lingutla Chandrasekhar <clingutla@codeaurora.org>

--- arch/arm64/kernel/cpuinfo.c
+++ arch/arm64/kernel/cpuinfo.c
@@ -147,6 +147,11 @@ static int c_show(struct seq_file *m, void *v)
 		seq_printf(m, "CPU revision\t: %d\n\n", MIDR_REVISION(midr));
 	}
 
+	if (!arch_read_hardware_id)
+		seq_printf(m, "Hardware\t: %s\n", machine_name);
+	else
+		seq_printf(m, "Hardware\t: %s\n", arch_read_hardware_id());
+
 	return 0;
 }
 
