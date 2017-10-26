c44fdc33eead00c058cf001a20a60b11f74a8543
Tanya Dixit <tdixit@codeaurora.org>
2017-10-26 13:50:42 +0530

    drivers: qdsp6v2: Add mutex unlock to properly release lock
    Add mutex unlock in function audio_effects_shared_ioctl
    at appropriate place to prevent use after free.
    
    CRs-Fixed: 2123291
    Change-Id: Ie0d321dc8cc20a295d102a44faea7e5710834932
    Signed-off-by: Tanya Dixit <tdixit@codeaurora.org>

--- drivers/misc/qcom/qdsp6v2/audio_hwacc_effects.c
+++ drivers/misc/qcom/qdsp6v2/audio_hwacc_effects.c
@@ -160,7 +160,6 @@ static int audio_effects_shared_ioctl(struct file *file, unsigned cmd,
 			pr_err("%s: Read buffer Allocation failed rc = %d\n",
 				__func__, rc);
 			rc = -ENOMEM;
-			mutex_unlock(&effects->lock);
 			goto readbuf_fail;
 		}
 		atomic_set(&effects->out_count, effects->config.output.num_buf);
@@ -175,7 +174,6 @@ static int audio_effects_shared_ioctl(struct file *file, unsigned cmd,
 		if (rc < 0) {
 			pr_err("%s: pcm read block config failed\n", __func__);
 			rc = -EINVAL;
-			mutex_unlock(&effects->lock);
 			goto cfg_fail;
 		}
 		pr_debug("%s: dec: sample_rate: %d, num_channels: %d, bit_width: %d\n",
@@ -190,7 +188,6 @@ static int audio_effects_shared_ioctl(struct file *file, unsigned cmd,
 			pr_err("%s: pcm write format block config failed\n",
 				__func__);
 			rc = -EINVAL;
-			mutex_unlock(&effects->lock);
 			goto cfg_fail;
 		}
 
@@ -323,6 +320,7 @@ ioctl_fail:
 readbuf_fail:
 	q6asm_audio_client_buf_free_contiguous(IN,
 					effects->ac);
+	mutex_unlock(&effects->lock);
 	return rc;
 cfg_fail:
 	q6asm_audio_client_buf_free_contiguous(IN,
@@ -330,6 +328,7 @@ cfg_fail:
 	q6asm_audio_client_buf_free_contiguous(OUT,
 					effects->ac);
 	effects->buf_alloc = 0;
+	mutex_unlock(&effects->lock);
 	return rc;
 }
 
