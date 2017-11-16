123ffa2401de85a59b8c9f7e837f5c05fde3df45
smanag <smanag@codeaurora.org>
2017-11-16 13:42:12 +0530

    ASoC: msm8x16-wcd: Read subsystem name from dtsi
    msm8x16-wcd codec registers callback with adsp
    subsystem to receive subsystem restart notifications.
    This will not work for msm8909 and msm8916 targets
    where audio dsp firmware is run on modem dsp.
    
    Read subsystem name from codec node of dtsi and
    register callback with the respective subsystem.
    msm8916, msm8909 are expected to have this subsys-name
    property defined in the codec node.
    
    CRs-Fixed: 2127307
    Change-Id: I7a2d98513b9a32ed9f0af895820113d627e65813
    Signed-off-by: Soumya Managoli <smanag@codeaurora.org>

--- sound/soc/codecs/msm8x16-wcd.c
+++ sound/soc/codecs/msm8x16-wcd.c
@@ -5808,6 +5808,7 @@ static int msm8x16_wcd_codec_probe(struct snd_soc_codec *codec)
 	struct msm8x16_wcd *msm8x16_wcd;
 	struct msm8x16_wcd_pdata *pdata;
 	int i, ret;
+	const char *subsys_name = NULL;
 
 	audio_dsm_register();
 	dev_dbg(codec->dev, "%s()\n", __func__);
@@ -5944,9 +5945,19 @@ static int msm8x16_wcd_codec_probe(struct snd_soc_codec *codec)
 	msm8x16_wcd_configure_cap(codec, false, false);
 	registered_codec = codec;
 	hw_get_registered_codec(registered_codec);
-	adsp_state_notifier =
-	    subsys_notif_register_notifier("adsp",
-					   &adsp_state_notifier_block);
+	ret = of_property_read_string(codec->dev->of_node,
+				"qcom,subsys-name",
+				&subsys_name);
+	if (ret) {
+		dev_dbg(codec->dev, "missing subsys-name entry in dt node\n");
+		adsp_state_notifier =
+			subsys_notif_register_notifier("adsp",
+			&adsp_state_notifier_block);
+	} else {
+		adsp_state_notifier =
+			subsys_notif_register_notifier(subsys_name,
+			&adsp_state_notifier_block);
+	}
 	if (!adsp_state_notifier) {
 		audio_dsm_report_num(DSM_AUDIO_CARD_LOAD_FAIL_ERROR_NO,
 								DSM_AUDIO_MESG_MEDOM_REGIST_FAIL);
