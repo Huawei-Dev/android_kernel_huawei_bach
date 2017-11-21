3bed48e6c5c1d460b8bead4886bca0689fd15752
Kyle Yan <kyan@codeaurora.org>
2017-11-20 16:43:37 -0800

    soc: qcom: pil: Do not panic on powerup if the system is rebooting
    When SSR occurs during reboot, ueventd may be killed which can cause
    request_firmware in pil_boot to return an error. Do not panic in this
    case as the request_firmware call is expected to fail.
    
    Change-Id: I6bfdf4d9a634dec1aa56a1fdc5553cefe4a94a72
    Signed-off-by: Kyle Yan <kyan@codeaurora.org>
    Signed-off-by: Jitendra Sharma <shajit@codeaurora.org>

--- drivers/soc/qcom/subsystem_restart.c
+++ drivers/soc/qcom/subsystem_restart.c
@@ -648,7 +648,16 @@ static void subsystem_powerup(struct subsys_device *dev, void *data)
 	if (dev->desc->powerup(dev->desc) < 0) {
 		notify_each_subsys_device(&dev, 1, SUBSYS_POWERUP_FAILURE,
 								NULL);
-		panic("[%p]: Powerup error: %s!", current, name);
+		if (system_state == SYSTEM_RESTART
+			|| system_state == SYSTEM_POWER_OFF)
+			WARN(1, "SSR aborted: %s, system reboot/shutdown is under way\n",
+				name);
+		else if (!dev->desc->ignore_ssr_failure)
+			panic("[%s:%d]: Powerup error: %s!",
+				current->comm, current->pid, name);
+		else
+			pr_err("Powerup failure on %s\n", name);
+		return ret;
 	}
 	enable_all_irqs(dev);
 
