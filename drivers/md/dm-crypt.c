60968bd67dae11c3c90f2197281b5df1326216a8
Artem Labazov <123321artyom@gmail.com>
2018-04-13 18:57:13 +0300

    dm-crypt: Fix QCOM mismerges
    Signed-off-by: Artem Labazov <123321artyom@gmail.com>

--- drivers/md/dm-crypt.c
+++ drivers/md/dm-crypt.c
@@ -1860,10 +1860,7 @@ static int crypt_ctr(struct dm_target *ti, unsigned int argc, char **argv)
 	}
 
 	ret = -ENOMEM;
-	cc->io_queue = alloc_workqueue("kcryptd_io",
-				       WQ_HIGHPRI |
-				       WQ_MEM_RECLAIM,
-				       1);
+	cc->io_queue = alloc_workqueue("kcryptd_io", WQ_HIGHPRI | WQ_MEM_RECLAIM, 1);
 	if (!cc->io_queue) {
 		ti->error = "Couldn't create kcryptd io queue";
 		goto bad;
