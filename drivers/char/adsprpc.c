6ee04b7a7953fd9fd854eb2a8fbe6440b5add5e1
Huawei-Dev <surdu_petru@yahoo.com>
2018-12-30 11:52:19 +0100

    msm8937: fix compiling issues

--- drivers/char/adsprpc.c
+++ drivers/char/adsprpc.c
@@ -2158,17 +2158,6 @@ static long fastrpc_device_ioctl(struct file *file, unsigned int ioctl_num,
 			break;
 		}
 		break;
-	case FASTRPC_IOCTL_GETINFO:
-	    K_COPY_FROM_USER(err, 0, &info, param, sizeof(info));
-		if (err)
-			goto bail;
-		VERIFY(err, 0 == (err = fastrpc_get_info(fl, &info)));
-		if (err)
-			goto bail;
-		K_COPY_TO_USER(err, 0, param, &info, sizeof(info));
-		if (err)
-			goto bail;
-		break;
 	case FASTRPC_IOCTL_INIT:
 		VERIFY(err, 0 == copy_from_user(&p.init, param,
 						sizeof(p.init)));
--- net/ipv4/raw.c
+++ net/ipv4/raw.c
@@ -589,8 +589,8 @@ static int raw_sendmsg(struct kiocb *iocb, struct sock *sk, struct msghdr *msg,
 			   RT_SCOPE_UNIVERSE,
 			   hdrincl ? IPPROTO_RAW : sk->sk_protocol,
 			   inet_sk_flowi_flags(sk) |
-			    (hdrincl ? FLOWI_FLAG_KNOWN_NH : 0),
-			   daddr, saddr, 0, 0);
+			    (inet->hdrincl ? FLOWI_FLAG_KNOWN_NH : 0),
+			   daddr, saddr, 0, 0, sk->sk_uid);
 
 	if (!hdrincl) {
 		err = raw_probe_proto_opt(&fl4, msg);
