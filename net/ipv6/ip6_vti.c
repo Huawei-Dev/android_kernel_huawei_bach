09993bce7a6fdb1251d53b9fc31cfb239aab9b74
Huawei-Dev <surdu_petru@yahoo.com>
2019-03-17 18:57:37 +0100

    msm8937:net:ipv6: Revert:Revert: net: ipv6: fix virtual tunnel build

--- net/ipv6/ip6_vti.c
+++ net/ipv6/ip6_vti.c
@@ -567,7 +567,7 @@ static int vti6_err(struct sk_buff *skb, struct inet6_skb_parm *opt,
 		ip6_redirect(skb, net, skb->dev->ifindex, 0,
 			     sock_net_uid(net, NULL));
 	else
-		ip6_update_pmtu(skb, net, info, 0, 0);
+		ip6_update_pmtu(skb, net, info, 0, 0, sock_net_uid(net, NULL));
 	xfrm_state_put(x);
 
 	return 0;
