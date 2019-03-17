9c15e329d9ccdf93f929badb037f14fc5cb85d51
Huawei-Dev <surdu_petru@yahoo.com>
2019-03-17 14:17:57 +0100

    msm8937:net:wireless: fix allocate space

--- drivers/net/wireless/wlan_feature.c
+++ drivers/net/wireless/wlan_feature.c
@@ -23,11 +23,11 @@
 #include <asm/uaccess.h>
 #include <linux/wcnss_wlan.h>
 
-#define WLAN_DEVICE_VERSION		"1.0"
-#define PROC_DIR	"wlan_feature"
+#define WLAN_DEVICE_VERSION	"1.0"
+#define PROC_DIR		"wlan_feature"
 
-#define WIFI_TYPE_UNKNOWN "Unknown"
-struct proc_dir_entry    *wlan_dir, *devtype_dir, *pubfd_dir;
+#define WIFI_TYPE_UNKNOWN	"Unknown"
+struct proc_dir_entry *wlan_dir, *devtype_dir, *pubfd_dir;
 
 static int wlan_featuretrans_remove(struct platform_device *pdev)
 {
@@ -73,31 +73,31 @@ const struct wifi_device wifi_device_array[] =
 char *get_wifi_device_type(void)
 {
     int i = 0;
-    int chip_type_len;
+    int chip_type_len = 0;
     int arry_size = sizeof(wifi_device_array)/sizeof(wifi_device_array[0]);
     const char *wifi_chip_type = WIFI_TYPE_UNKNOWN;
     struct device_node *dp = NULL;
     dp = of_find_node_by_path("/huawei_wifi_info");
-    if(!dp) {
+    if (!dp) {
         pr_err("device is not available!\n");
         return wifi_device_array[arry_size - 1].dev_name;
     }
 
     /* get wifi chip type from the device feature configuration (.dtsi file) */
-    wifi_chip_type = of_get_property(dp,"wifi,chiptype", &chip_type_len);
-    if(NULL == wifi_chip_type) {
+    wifi_chip_type = of_get_property(dp, "wifi,chiptype", &chip_type_len);
+    if (NULL == wifi_chip_type) {
         pr_err("WIFI, Get chip type fail.\n");
         return wifi_device_array[arry_size - 1].dev_name;
     }
 
     /* lookup wifi_device_model in wifi_device_array[] */
-    for(i = 0; i < arry_size; i++) {
-        if(0 == strncmp(wifi_chip_type,wifi_device_array[i].chip_type,chip_type_len)) {
+    for (i = 0; i < arry_size; i++) {
+        if (0 == strncmp(wifi_chip_type, wifi_device_array[i].chip_type, chip_type_len)) {
             break;
         }
     }
     /* If we get invalid type name, return "Unknown".*/
-    if( i == arry_size) {
+    if (i == arry_size) {
          pr_err("WIFI, Get chip type fail.\n");
          return wifi_device_array[arry_size - 1].dev_name;
     }
@@ -111,8 +111,8 @@ static int devtype_proc_show(struct seq_file *m, void *v)
     const char *wifi_device_type = NULL;
 
     wifi_device_type = get_wifi_device_type();
-    pr_debug("%s enter wifi_device_type:%s;\n", __func__,wifi_device_type);
-    seq_printf(m,"%s",wifi_device_type);
+    pr_debug("%s enter wifi_device_type:%s;\n", __func__, wifi_device_type);
+    seq_printf(m, "%s", wifi_device_type);
 
     return 0;
 }
@@ -122,7 +122,7 @@ static int devtype_proc_open(struct inode *inode, struct file *file)
 	return single_open(file, devtype_proc_show, NULL);
 }
 
- static const struct file_operations devtype_proc_fops = {
+static const struct file_operations devtype_proc_fops = {
 	.owner		= THIS_MODULE,
 	.open		= devtype_proc_open,
 	.read		= seq_read,
@@ -130,20 +130,20 @@ static int devtype_proc_open(struct inode *inode, struct file *file)
 	.release	= single_release,
 };
 
- const void *get_hw_wifi_pubfile_id(void)
+const void *get_hw_wifi_pubfile_id(void)
 {
-    int wifi_pubfile_id_len;
+    int wifi_pubfile_id_len = 0;
     struct device_node *dp = NULL;
 
     dp = of_find_node_by_path("/huawei_wifi_info");
-    if(!dp) {
+    if (!dp) {
          pr_err("device is not available!\n");
          return NULL;
-    }else {
-         pr_debug("%s:dp->name:%s,dp->full_name:%s;\n",__func__,dp->name,dp->full_name);
+    } else {
+         pr_debug("%s:dp->name:%s,dp->full_name:%s;\n", __func__, dp->name, dp->full_name);
     }
 
-    return  of_get_property(dp,"wifi,pubfile_id", &wifi_pubfile_id_len);
+    return of_get_property(dp, "wifi,pubfile_id", &wifi_pubfile_id_len);
 }
 EXPORT_SYMBOL(get_hw_wifi_pubfile_id);
 
@@ -153,14 +153,14 @@ static int pubfd_proc_show(struct seq_file *m, void *v)
 
     wifi_pubfile_id = get_hw_wifi_pubfile_id();
 
-    if( NULL != wifi_pubfile_id ) {
-        pr_debug("%s enter wifi_device_type:%s;\n", __func__,wifi_pubfile_id);
-    }else {
+    if (NULL != wifi_pubfile_id ) {
+        pr_debug("%s enter wifi_device_type:%s;\n", __func__, wifi_pubfile_id);
+    } else {
         pr_err("%s get pubfd failed;\n", __func__);
         return 0;
     }
 
-    seq_printf(m,"%s",wifi_pubfile_id);
+    seq_printf(m, "%s", wifi_pubfile_id);
 
     return 0;
 }
@@ -180,17 +180,17 @@ static int pubfd_proc_open(struct inode *inode, struct file *file)
 
 const void *get_wlan_fw_ver(void)
 {
-    int wlan_fw_ver_len;
+    int wlan_fw_ver_len = 0;
     struct device_node *dp = NULL;
     dp = of_find_node_by_path("/huawei_wifi_info");
-    if(!dp) {
+    if (!dp) {
          pr_err("device is not available!\n");
          return NULL;
-    }else {
-         pr_debug("%s:dp->name:%s,dp->full_name:%s;\n",__func__,dp->name,dp->full_name);
+    } else {
+         pr_debug("%s:dp->name:%s,dp->full_name:%s;\n", __func__, dp->name, dp->full_name);
     }
 
-    return  of_get_property(dp,"wifi,fw_ver", &wlan_fw_ver_len);
+    return  of_get_property(dp, "wifi,fw_ver", &wlan_fw_ver_len);
 }
 
 static int wlan_fw_ver_proc_show(struct seq_file *m, void *v)
@@ -199,14 +199,14 @@ static int wlan_fw_ver_proc_show(struct seq_file *m, void *v)
 
     wlan_fw_ver = get_wlan_fw_ver();
 
-    if( NULL !=wlan_fw_ver ) {
-         pr_debug("%s enter wifi_device_type:%s;\n", __func__,wlan_fw_ver);
-    }else {
+    if (NULL != wlan_fw_ver) {
+         pr_debug("%s enter wifi_device_type:%s;\n", __func__, wlan_fw_ver);
+    } else {
         pr_err("%s get wlan_fw_ver failed;\n", __func__);
         return 0;
     }
 
-    seq_printf(m,"%s",wlan_fw_ver);
+    seq_printf(m, "%s", wlan_fw_ver);
 
     return 0;
 }
@@ -227,32 +227,32 @@ static int wlan_fw_ver_proc_open(struct inode *inode, struct file *file)
 const int get_hw_wifi_no_autodetect_xo(void)
 {
     struct device_node *dp = NULL;
-	int ret = -1;
+    int ret = -1;
 
     dp = of_find_node_by_path("/huawei_wifi_info");
-    if(!dp) {
+    if (!dp) {
          pr_err("device is not available!\n");
          return ret;
-    }else {
-         pr_debug("%s:dp->name:%s,dp->full_name:%s;\n",__func__,dp->name,dp->full_name);
+    } else {
+         pr_debug("%s:dp->name:%s,dp->full_name:%s;\n", __func__, dp->name, dp->full_name);
     }
 
-    return  of_property_read_bool(dp,"wifi,no-autodetect-xo");
+    return of_property_read_bool(dp, "wifi,no-autodetect-xo");
 }
 EXPORT_SYMBOL(get_hw_wifi_no_autodetect_xo);
 
 const void *get_fem_check_flag(void)
 {
-    int get_fem_check_len;
+    int get_fem_check_len = 0;
     struct device_node *dp = NULL;
 
     dp = of_find_node_by_path("/huawei_wifi_info");
     if (!dp) {
-        pr_err("%s:device is not available!\n",__func__);
+        pr_err("%s:device is not available!\n", __func__);
         return NULL;
     }
 
-    return  of_get_property(dp,"wifi,fem_check_flag", &get_fem_check_len);
+    return of_get_property(dp, "wifi,fem_check_flag", &get_fem_check_len);
 }
 EXPORT_SYMBOL(get_fem_check_flag);
 
@@ -261,14 +261,14 @@ static int fen_check_flag_show(struct seq_file *m, void *v)
     const char *fem_check_flag = NULL;
 
     fem_check_flag = get_fem_check_flag();
-    if ( NULL != fem_check_flag ) {
-        pr_err("%s enter fem_check_flag:%s;\n", __func__,fem_check_flag);
+    if (NULL != fem_check_flag) {
+        pr_err("%s enter fem_check_flag:%s;\n", __func__, fem_check_flag);
     } else {
         pr_err("%s get fem_check_flag failed;\n", __func__);
         return 0;
     }
 
-    seq_printf(m,"%s",fem_check_flag);
+    seq_printf(m, "%s", fem_check_flag);
 
     return 0;
 }
@@ -287,16 +287,16 @@ static int fem_check_flag_open(struct inode *inode, struct file *file)
 };
 const void *get_fem_check_5g_flag(void)
 {
-    int get_fem_check_5g_len;
+    int get_fem_check_5g_len = 0;
     struct device_node *dp = NULL;
 
     dp = of_find_node_by_path("/huawei_wifi_info");
-    if(!dp) {
+    if (!dp) {
         pr_err("get_fem_check_5g_flag:device is not available!\n");
         return NULL;
     }
 
-    return  of_get_property(dp,"wifi,fem_check_5g_flag", &get_fem_check_5g_len);
+    return of_get_property(dp, "wifi,fem_check_5g_flag", &get_fem_check_5g_len);
 }
 EXPORT_SYMBOL(get_fem_check_5g_flag);
 
@@ -305,14 +305,14 @@ static int fem_check_5g_flag_show(struct seq_file *m, void *v)
     const char *fem_check_5g_flag = NULL;
 
     fem_check_5g_flag = get_fem_check_5g_flag();
-    if( NULL != fem_check_5g_flag ){
+    if (NULL != fem_check_5g_flag) {
         pr_err("get_fem_check_5g_flag: enter fem_check_5g_flag:%s;\n", fem_check_5g_flag);
     } else {
         pr_err("get_fem_check_5g_flag get fem_check_5g_flag failed;\n");
         return 0;
     }
 
-    seq_printf(m,"%s",fem_check_5g_flag);
+    seq_printf(m, "%s", fem_check_5g_flag);
 
     return 0;
 }
@@ -333,17 +333,17 @@ static const struct file_operations fem_check_5g_flag_proc_fops = {
 
 const void *get_hw_wifi_ini_type(void)
 {
-    int wifi_ini_type_len;
+    int wifi_ini_type_len = 0;
     struct device_node *dp = NULL;
 
     dp = of_find_node_by_path("/huawei_wifi_info");
-    if(!dp) {
+    if (!dp) {
          pr_err("device is not available!\n");
          return NULL;
-    }else {
-         pr_debug("%s:dp->name:%s,dp->full_name:%s;\n",__func__,dp->name,dp->full_name);
+    } else {
+         pr_debug("%s:dp->name:%s,dp->full_name:%s;\n", __func__, dp->name, dp->full_name);
     }
-    return  of_get_property(dp,"wifi,ini_type", &wifi_ini_type_len);
+    return of_get_property(dp, "wifi,ini_type", &wifi_ini_type_len);
 }
 EXPORT_SYMBOL(get_hw_wifi_ini_type);
 
@@ -353,14 +353,14 @@ static int ini_type_proc_show(struct seq_file *m, void *v)
 
     wifi_ini_type = get_hw_wifi_ini_type();
 
-    if( NULL != wifi_ini_type ) {
-        pr_debug("%s enter wifi_device_type:%s;\n", __func__,wifi_ini_type);
-    }else {
+    if (NULL != wifi_ini_type) {
+        pr_debug("%s enter wifi_device_type:%s;\n", __func__, wifi_ini_type);
+    } else {
         pr_err("%s get pubfd failed;\n", __func__);
         return 0;
     }
 
-    seq_printf(m,"%s",wifi_ini_type);
+    seq_printf(m, "%s", wifi_ini_type);
 
     return 0;
 }
@@ -396,55 +396,55 @@ static int __init wlanfeaturetrans_init(void)
     wlan_dir = proc_mkdir("wlan_feature", NULL);
     if (NULL == wlan_dir) {
         pr_err("Unable to create /proc/wlan_feature directory");
-        retval =  -ENOMEM;
+        retval = -ENOMEM;
         goto error_dir;
     }
 
     /* Creating read/write "devtype" entry*/
-    ent= proc_create("devtype", 0, wlan_dir, &devtype_proc_fops);
+    ent = proc_create("devtype", 0, wlan_dir, &devtype_proc_fops);
     if (!ent) {
         pr_err("Unable to create /proc/%s/devtype entry", PROC_DIR);
-        retval =  -ENOMEM;
+        retval = -ENOMEM;
         goto error_devtype;
     }
 
     /* Creating read/write "pubfd" entry*/
-    ent= proc_create("pubfd", 0, wlan_dir, &pubfd_proc_fops);
+    ent = proc_create("pubfd", 0, wlan_dir, &pubfd_proc_fops);
     if (!ent) {
         pr_err("Unable to create /proc/%s/pubfd entry", PROC_DIR);
-        retval =  -ENOMEM;
+        retval = -ENOMEM;
         goto error_pubfd;
     }
 
     /* Creating read/write "wlan_fw_ver" entry for wifi chip type*/
-    ent= proc_create("wlan_fw_ver", 0, wlan_dir, &wlan_fw_ver_proc_fops);
+    ent = proc_create("wlan_fw_ver", 0, wlan_dir, &wlan_fw_ver_proc_fops);
     if (!ent) {
         pr_err("Unable to create /proc/%s/wlan_fw_ver entry", PROC_DIR);
-        retval =  -ENOMEM;
+        retval = -ENOMEM;
         goto error_wlan_fw_ver;
     }
 
     /* Creating read/write "fem_check_flag" entry for wifi chip type*/
-    ent= proc_create("fem_check_flag", 0, wlan_dir, &fem_check_flag_proc_fops);
+    ent = proc_create("fem_check_flag", 0, wlan_dir, &fem_check_flag_proc_fops);
     if (!ent)
     {
-        pr_err("%s:Unable to create /proc/%s/fem_check_flag entry",__func__,PROC_DIR);
-        retval =  -ENOMEM;
+        pr_err("%s:Unable to create /proc/%s/fem_check_flag entry", __func__, PROC_DIR);
+        retval = -ENOMEM;
         goto error_fem_check_flag;
     }
     /* Creating read/write "fem_check_5g_flag" entry for wifi chip type*/
-    ent= proc_create("fem_check_5g_flag", 0, wlan_dir, &fem_check_5g_flag_proc_fops);
+    ent = proc_create("fem_check_5g_flag", 0, wlan_dir, &fem_check_5g_flag_proc_fops);
     if (!ent)
     {
-        pr_err("%s:Unable to create /proc/%s/fem_check_5g_flag entry",__func__,PROC_DIR);
-        retval =  -ENOMEM;
+        pr_err("%s:Unable to create /proc/%s/fem_check_5g_flag entry", __func__, PROC_DIR);
+        retval = -ENOMEM;
         goto error_fem_check_5g_flag;
     }
     /* Creating read/write "ini_type" entry*/
-    ent= proc_create("ini_type", 0, wlan_dir, &ini_type_proc_fops);
+    ent = proc_create("ini_type", 0, wlan_dir, &ini_type_proc_fops);
     if (!ent) {
         pr_err("Unable to create /proc/%s/ini_type entry", PROC_DIR);
-        retval =  -ENOMEM;
+        retval = -ENOMEM;
         goto error_ini_type;
     }
     return 0;
