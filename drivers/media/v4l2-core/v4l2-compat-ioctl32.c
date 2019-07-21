50bc449f2b9f8df7bd661fbfbcfbcc0462114965
Huawei-Dev <surdu_petru@yahoo.com>
2019-07-21 12:34:58 +0200

    msm8937: update for V4L2_TYPE_IS_PRIVATE

--- drivers/media/v4l2-core/v4l2-compat-ioctl32.c
+++ drivers/media/v4l2-core/v4l2-compat-ioctl32.c
@@ -508,8 +508,8 @@ static int get_v4l2_buffer32(struct v4l2_buffer __user *kp,
 	if (V4L2_TYPE_IS_PRIVATE(type)) {
 		compat_ulong_t userptr;
 
-		if (get_user(length, &up->length) || get_user(userptr, &up->m.userptr) ||
-				put_user((unsigned long)compat_ptr(userptr),
+		if (get_user(length, &up->length) || put_user(length, &kp->length) ||
+				get_user(userptr, &up->m.userptr) || put_user((unsigned long)compat_ptr(userptr),
 				     &kp->m.userptr))
 				return -EFAULT;
 	}
