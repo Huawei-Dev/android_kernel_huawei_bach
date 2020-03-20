db9f01a8c99987c66704867547e795c546bd476d
Huawei-Dev <surdu_petru@yahoo.com>
2020-03-20 10:49:52 +0100

    bach:fs: remove unused variable

--- fs/pstore/ram_core.c
+++ fs/pstore/ram_core.c
@@ -301,7 +301,6 @@ ssize_t persistent_ram_ecc_string(struct persistent_ram_zone *prz,
 static void notrace persistent_ram_update(struct persistent_ram_zone *prz,
 	const void *s, unsigned int start, unsigned int count)
 {
-	struct persistent_ram_buffer *buffer = prz->buffer;
 	persistent_ram_update_ecc(prz, start, count);
 }
 
